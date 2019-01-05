e63807b9e2325435a410c3e7d37d980fcd8bd6f9
Forland <frankforland@gmail.com>
2019-01-05 16:09:43 +0000

    cleanUp  and navbar

--- forlands/src/App.vue
+++ forlands/src/App.vue
@@ -1,56 +1,15 @@
 <template>
   <div id="app">
-    
-<b-navbar toggleable="md" type="dark" variant="info" fixed="top">
-
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
-  <b-navbar-brand><router-link to="Hello">Home</router-link></b-navbar-brand>
-
-  <b-collapse is-nav id="nav_collapse">
-
-    <b-navbar-nav>
-      <b-nav-item><router-link to="/" exact>tables</router-link></b-nav-item>
-      <b-nav-item disabled><router-link to="Hello">HEllo</router-link></b-nav-item>
-      <b-nav-item disabled><router-link to="Front">Front</router-link></b-nav-item>
-      <b-nav-item disabled><router-link to="Carousel">Carousel</router-link></b-nav-item>
-      <b-nav-item><router-link to="GetIp">GetIp</router-link></b-nav-item>
-    </b-navbar-nav>
-
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-
-      <b-nav-form>
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-      </b-nav-form>
-
-      <b-nav-item-dropdown text="Lang" right>
-        <b-dropdown-item href="#">EN</b-dropdown-item>
-        <b-dropdown-item href="#">ES</b-dropdown-item>
-        <b-dropdown-item href="#">RU</b-dropdown-item>
-        <b-dropdown-item href="#">FA</b-dropdown-item>
-      </b-nav-item-dropdown>
-
-      <b-nav-item-dropdown right>
-        <!-- Using button-content slot -->
-        <template slot="button-content">
-          <em>User</em>
-        </template>
-        <b-dropdown-item href="#">Profile</b-dropdown-item>
-        <b-dropdown-item href="#">Signout</b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar-nav>
-
-  </b-collapse>
-</b-navbar>
-    <!--<img src="./assets/FotoJet.png">-->
+    <NavBarMenu/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavBarMenu from './components/NavBarMenu';
+
 export default {
+  components: { NavBarMenu },
   name: 'app',
 };
 </script>
--- forlands/src/router/index.js
+++ forlands/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
-import FrontPage from '@/components/FrontPage';
+import Play from '@/components/Play';
 import Carousel from '@/components/Carousel';
 import Tables from '@/components/Tables';
 import GetIp from '@/components/GetIp';
@@ -22,19 +21,14 @@ export default new Router({
       component: Tables,
     },
     {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello,
-    },
-    {
       path: '/getip',
       name: 'GetIp',
       component: GetIp,
     },
     {
-      path: '/front',
-      name: 'Front',
-      component: FrontPage,
+      path: '/play',
+      name: 'Play',
+      component: Play,
     },
     {
       path: '/carousel',
